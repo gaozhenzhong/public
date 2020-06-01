a1f5b9543ec8c25c6ff76b3020e10ddd9c3aa664
gaozhenzhong <314gaozhenzhong@163.com>
2020-06-01 17:14:02 +0800

    实现dev代码命令发送

--- qt_linux/codecAndServer/codecDev/codecDev/codecDev.cpp
+++ qt_linux/codecAndServer/codecDev/codecDev/codecDev.cpp
@@ -17,6 +17,7 @@ struct registOrder
 }    ;
 
 codecDev::orderData::orderData(char id,char* _data,char _size)
+    :len_( 0 )
 {
 #if 0
     order_.insert(order_.end(),_size+10);
@@ -31,14 +32,19 @@ codecDev::orderData::orderData(char id,char* _data,char _size)
     order_.insert(order_.end(),'F');
     order_.insert(order_.end(),'F');
 #else
-    //std::memcpy(order_,"FF55FF55",sizeof("FF55FF55"));
-
+    memcpy(order_,orderHeadStr,orderHeadLen);
+    order_[orderLenAddr] = static_cast<char>(_size+1);//id+data 的长度
+    order_[orderIDAddr]  = static_cast<char>(id);
+    memcpy(order_+orderIDDataAddr,_data,_size);
+    memcpy(order_+orderIDAddr+_size+1,orderTailStr,orderTailLen);
+    len_ = orderIDAddr+1+_size+orderTailLen;
 #endif
 }
 
 codecDev::codecDev(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::codecDev)
+    ,tcpSocket(NULL)
 {
     ui->setupUi(this);
     //设置界面的显示数据范围
@@ -54,50 +60,60 @@ codecDev::~codecDev()
 void codecDev::on_conPushButton_clicked()
 {
     bool ok;
-    QString ip = ui->IPLineEdit->text();
-    int port   = ui->portLineEdit->text().toInt(&ok);
-    QByteArray ba = ip.toLatin1();
-    bool isAddr =kBaseFunction::isCurrectIP(ba.data());
-    if(!isAddr)
+    if(tcpSocket == NULL)
     {
-       ui->logTextBrowser->append(ip+" is no ip!");
+        QString ip = ui->IPLineEdit->text();
+        int port   = ui->portLineEdit->text().toInt(&ok);
+        QByteArray ba = ip.toLatin1();
+        bool isAddr =kBaseFunction::isCurrectIP(ba.data());
+        if(!isAddr)
+        {
+           ui->logTextBrowser->append(ip+" is no ip!");
+        }
+        //分配空间，指定父对象
+        tcpSocket = new QTcpSocket(this);
+
+        tcpSocket->connectToHost(QHostAddress(ip), port);
+
+        ui->conPushButton->setText("disconnet");
+
+        connect(tcpSocket, &QTcpSocket::connected,
+            [=]()
+            {
+                struct registOrder data;
+                data.devType   = 1;
+                codecDev::orderData order(static_cast<char>(REGIST) ,reinterpret_cast<char*>(&data),static_cast<char>(sizeof(data)));
+                tcpSocket->write(order.order_,order.len_);
+                ui->logTextBrowser->append(QString("%1 : %2 : connect success").arg(ip).arg(port));
+            }
+        );
+
+        //因为tcpSocket已经分配了空间，有指向，所以可以放在外面
+        connect(tcpSocket, &QTcpSocket::readyRead,
+            [=]()
+            {
+                //获取对方发送的内容
+                QByteArray array = tcpSocket->readAll();
+                //追加到编辑区中
+                ui->logTextBrowser->append(QString("%1 : %2 : rec: %3").arg(ip).arg(port).arg(array.data()));
+                //todo:添加数据函数
+                process_netCmd(array);
+            }
+        );
+
+        connect(tcpSocket, &QTcpSocket::disconnected,
+            [=]()
+            {
+                ui->logTextBrowser->append(QString("%1 : %2 :disconnected").arg(ip).arg(port));
+            }
+        );
     }
-    //分配空间，指定父对象
-    tcpSocket = std::unique_ptr<QTcpSocket>(new QTcpSocket(this));
-    tcpSocket->connectToHost(QHostAddress(ip), port);
-
-
-    connect(tcpSocket.get(), &QTcpSocket::connected,
-        [=]()
-        {
-            struct registOrder data;
-            data.devType   = 1;
-            codecDev::orderData order(static_cast<char>(REGIST) ,reinterpret_cast<char*>(&data),static_cast<char>(sizeof(data)));
-            //tcpSocket->write(codecDev::orderData.order_.,);
-            ui->logTextBrowser->append(QString("%1 : %2 : connect success").arg(ip).arg(port));
-
-        }
-    );
-
-    //因为tcpSocket已经分配了空间，有指向，所以可以放在外面
-    connect(tcpSocket.get(), &QTcpSocket::readyRead,
-        [=]()
-        {
-            //获取对方发送的内容
-            QByteArray array = tcpSocket->readAll();
-            //追加到编辑区中
-            ui->logTextBrowser->append(QString("%1 : %2 : rec: %3").arg(ip).arg(port).arg(array.data()));
-            //todo:添加数据函数
-            process_netCmd(array);
-        }
-    );
-
-    connect(tcpSocket.get(), &QTcpSocket::disconnected,
-        [=]()
-        {
-            ui->logTextBrowser->append(QString("%1 : %2 :disconnected").arg(ip).arg(port));
-        }
-    );
+    else
+    {
+        delete tcpSocket;
+        tcpSocket = NULL;
+        ui->conPushButton->setText("connet");
+    }
 //void stateChanged(QAbstractSocket::SocketState);
 //void error(QAbstractSocket::SocketError);
 }
--- qt_linux/codecAndServer/codecDev/codecDev/codecDev.h
+++ qt_linux/codecAndServer/codecDev/codecDev/codecDev.h
@@ -17,10 +17,17 @@ private:
     {
     public:
         orderData(char id,char* _data,char _size);
-    private:
-        //std::vector<char> order_{'5','5','F','F','5','5','F','F'};
-        char order_;
-        int  len;
+
+        const char *orderHeadStr     = "FF55FF55";
+        const char orderHeadLen      = 8;
+        const int  orderLenAddr      = 8;
+        const int  orderIDAddr       = 9;
+        const char orderIDDataAddr   = 10;
+        const char *orderTailStr     = "55FF55FF";
+        const char orderTailLen      = 8;
+
+        char order_[300];
+        int  len_;
     };
 public:
     codecDev(QWidget *parent = nullptr);
@@ -30,7 +37,7 @@ private slots:
     void on_sendOrderButton_clicked();
 private:
     Ui::codecDev *ui;
-    std::unique_ptr<QTcpSocket> tcpSocket;
+    QTcpSocket  * tcpSocket;
     bool process_netCmd(QByteArray & _bArray);
 };
 #endif // CODECDEV_H
